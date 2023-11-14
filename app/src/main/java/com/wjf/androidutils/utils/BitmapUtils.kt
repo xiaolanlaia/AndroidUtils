6de403074dd1e447e04242dc18ad12896996f816
WuJianFeng <946372869@qq.com>
2023-11-14 09:56:55 +0800

    init: 初始化

---
+++ app/src/main/java/com/wjf/androidutils/utils/BitmapUtils.kt
@@ -0,0 +1,11 @@
+package com.wjf.androidutils.utils
+
+/**
+ * @Description
+ * @Author WuJianFeng
+ * @Date 2023/11/14 9:50
+ *
+ */
+
+object BitmapUtils {
+}
---
+++ app/src/main/java/com/wjf/androidutils/utils/ByteUtils.kt
@@ -0,0 +1,11 @@
+package com.wjf.androidutils.utils
+
+/**
+ * @Description
+ * @Author WuJianFeng
+ * @Date 2023/11/14 9:51
+ *
+ */
+
+class ByteUtils {
+}
