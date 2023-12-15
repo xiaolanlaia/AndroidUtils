6aed47b91bc5b143f2aae0d728a149abeb8006e5
WuJianFeng <946372869@qq.com>
2023-12-15 16:55:57 +0800

    feat: MVVMBaseActivity

--- app/src/main/java/com/wjf/androidutils/base/transit/TitleBarActivity.kt
+++ app/src/main/java/com/wjf/androidutils/base/transit/TitleBarActivity.kt
@@ -4,14 +4,13 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.wjf.androidutils.R
+import com.wjf.androidutils.base.MVVMBaseActivity
+import com.wjf.androidutils.databinding.ActivityTitleBarBinding
+import com.wjf.androidutils.databinding.CommonTitleBinding
 import com.wjf.androidutils.ui.AnimFragment
 import com.wjf.androidutils.ui.ArrayFragment
 import com.wjf.androidutils.ui.DesignFragment
@@ -47,10 +46,9 @@ import com.wjf.androidutils.utils.JUMP_TO_ToastFragment
 import com.wjf.androidutils.utils.JUMP_TO_WebViewFragment
 import com.wjf.moduleutils.PermissionUtil
 import com.wjf.moduleutils.ScreenUtils
-import com.wjf.moduleutils.StatusBar
 import com.wjf.moduleutils.singleClick
 
-class TitleBarActivity : AppCompatActivity() {
+class TitleBarActivity : MVVMBaseActivity<TitleBarViewModel,ActivityTitleBarBinding>() {
 
 
     companion object{
@@ -73,17 +71,16 @@ class TitleBarActivity : AppCompatActivity() {
     //请求状态码，请求码的作用是与回调函数进行匹配的，这样就可以对不同权限操作进行不同的提示
     val REQUEST_CODE = 1
 
-    lateinit var linearBack: LinearLayout
     lateinit var fragment: Fragment
-    lateinit var tvPageTitle: TextView
+    lateinit var commonTitleBinding: CommonTitleBinding
+
+    override fun initViewBinding(inflater: LayoutInflater) = ActivityTitleBarBinding.inflate(layoutInflater)
 
 
+    override fun initViewModel() = ViewModelProviders.of(this).get(TitleBarViewModel::class.java)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val statusBar = StatusBar(this)
-        statusBar.setColor(R.color.transparent)
-        setContentView(R.layout.activity_title_bar)
+    override fun initView(){
+        commonTitleBinding = CommonTitleBinding.bind(binding.root)
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU){
             permissionList.add(Manifest.permission.READ_EXTERNAL_STORAGE)
         }else{
@@ -93,122 +90,94 @@ class TitleBarActivity : AppCompatActivity() {
         }
         PermissionUtil.instance.requestPermissions(this,permissionList,REQUEST_CODE)
 
-        initView()
-        initClick()
-    }
-
-    private fun initView(){
-        linearBack = findViewById(R.id.linear_back)
-        tvPageTitle = findViewById(R.id.tv_page_title)
-        val commonTitle = findViewById<RelativeLayout>(R.id.common_title)
-        val viewStatus = findViewById<View>(R.id.view_status)
-        val layoutParams = viewStatus.layoutParams
+        val layoutParams = binding.viewStatus.layoutParams
         layoutParams.height = ScreenUtils.instance.getStatusBarHeight()
-        viewStatus.layoutParams = layoutParams
+        binding.viewStatus.layoutParams = layoutParams
 
         when(intent.getStringExtra(JUMP_TO)){
 
             JUMP_TO_DesignFragment -> {
-                tvPageTitle.text = "设计模式"
                 fragment = DesignFragment()
             }
 
             JUMP_TO_PersistentFragment -> {
-                tvPageTitle.text = "持久化存储"
                 fragment = PersistentFragment()
             }
 
             JUMP_TO_ToastFragment -> {
-                tvPageTitle.text = "Toast"
                 fragment = ToastFragment()
             }
 
             JUMP_TO_ArrayFragment -> {
-                tvPageTitle.text = "Array"
                 fragment = ArrayFragment()
             }
 
             JUMP_TO_FileFragment -> {
-                tvPageTitle.text = "FileFragment"
                 fragment = FileFragment()
             }
 
             JUMP_TO_ExceptionFragment -> {
-                tvPageTitle.text = "ExceptionFragment"
                 fragment = ExceptionFragment()
             }
 
             JUMP_TO_ReflectFragment -> {
-                tvPageTitle.text = "ReflectFragment"
                 fragment = ReflectFragment()
             }
 
             JUMP_TO_ImgLoaderFragment -> {
-                tvPageTitle.text = "ImgLoaderFragment"
                 fragment = ImgLoaderFragment()
             }
 
             JUMP_TO_WebViewFragment -> {
-                tvPageTitle.text = "WebViewFragment"
                 fragment = WebViewFragment()
             }
 
             JUMP_TO_AnimFragment -> {
-                tvPageTitle.text = "AnimFragment"
                 fragment = AnimFragment()
             }
 
             JUMP_TO_BlueFragment -> {
-                tvPageTitle.text = "BlueFragment"
                 fragment = BlueFragment()
             }
 
             JUMP_TO_BlueClientFragment -> {
-                tvPageTitle.text = "BlueClientFragment"
                 fragment = BlueClientFragment()
             }
 
             JUMP_TO_BlueServiceFragment -> {
-                tvPageTitle.text = "BlueServiceFragment"
                 fragment = BlueServiceFragment()
             }
 
 
             JUMP_TO_BleClientFragment -> {
-                tvPageTitle.text = "BleClientFragment"
                 fragment = BleClientFragment()
             }
 
             JUMP_TO_BleServiceFragment -> {
-                tvPageTitle.text = "BleServiceFragment"
                 fragment = BleServiceFragment()
             }
 
             else -> {
-                commonTitle.visibility = View.GONE
                 fragment = HomeFragment()
             }
 
         }
 
+        commonTitleBinding.tvPageTitle.text = fragment.javaClass.simpleName
         supportFragmentManager.beginTransaction().replace(R.id.info_content, fragment).commit()
 
     }
 
-    fun initClick(){
+    override fun initClick(){
 
-        linearBack.singleClick {
+        commonTitleBinding.linearBack.singleClick {
 
-
-            onBackPressed()
+            finish()
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
