e3959be3ae4f43acd24cfa09c50fcc523b2afab7
WuJianFeng <946372869@qq.com>
2024-01-07 21:30:37 +0800

    feat: Compose透明状态栏

--- app/src/main/java/com/wjf/androidutils/compose/ui/ComposeActivity.kt
+++ app/src/main/java/com/wjf/androidutils/compose/ui/ComposeActivity.kt
@@ -8,7 +8,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.wjf.androidutils.compose.nav.MyNavHost
 import com.wjf.androidutils.compose.theme.MyAppTheme
 
@@ -29,12 +34,19 @@ class ComposeActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             MyAppTheme{
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    MyNavHost()
+                ProvideWindowInsets {
+                    val systemUiController = rememberSystemUiController()
+                    SideEffect {
+                        systemUiController.setStatusBarColor(Color.Transparent, darkIcons = false)
+                    }
+
+                    Surface(modifier = Modifier.fillMaxSize()) {
+                        MyNavHost()
+                    }
                 }
             }
         }
