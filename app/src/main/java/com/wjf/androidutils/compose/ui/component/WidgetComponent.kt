ecd0437d336d7fc58958fe5c628c01112b502aad
WuJianFeng <946372869@qq.com>
2024-01-08 09:22:28 +0800

    feat: WidgetComponent

---
+++ app/src/main/java/com/wjf/androidutils/compose/ui/component/WidgetComponent.kt
@@ -0,0 +1,20 @@
+package com.wjf.androidutils.compose.ui.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+/**
+ * @Description
+ * @Author WuJianFeng
+ * @Date 2023/12/31 8:36
+ *
+ */
+
+@Composable
+fun WidgetComponent(
+    navHostController : NavHostController,
+    modifier: Modifier = Modifier
+) {
+
+}
