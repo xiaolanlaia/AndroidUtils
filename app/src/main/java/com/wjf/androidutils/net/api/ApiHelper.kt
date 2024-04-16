2b820ebeff461cbcf4622cc361b5f4fe06f49f40
WuJianFeng <946372869@qq.com>
2024-04-16 16:38:42 +0800

    workflow: 网络请求-上传图片及文字参数示例

--- app/src/main/java/com/wjf/androidutils/net/api/ApiHelper.kt
+++ app/src/main/java/com/wjf/androidutils/net/api/ApiHelper.kt
@@ -1,6 +1,28 @@
 package com.wjf.androidutils.net.api
 
+import okhttp3.MultipartBody
+
 object ApiHelper {
 
     suspend fun getUsers() = RetrofitBuilder.apiService.getUsers()
+
+    /**
+     * 上传图片文件及文字参数
+     */
+    suspend fun uploadCheckPicture(
+        imageBodyPart: MultipartBody.Part,
+        bucketBodyPart: MultipartBody.Part,
+        typeBodyPart: MultipartBody.Part,
+        sortBodyPart: MultipartBody.Part,
+        nameBodyPart: MultipartBody.Part,
+        checkIdBodyPart: MultipartBody.Part
+    ) = RetrofitBuilder.apiService.uploadCheckPicture(
+        imageBodyPart,
+        bucketBodyPart,
+        typeBodyPart,
+        sortBodyPart,
+        nameBodyPart,
+        checkIdBodyPart
+    )
+
 }
--- app/src/main/java/com/wjf/androidutils/net/api/ApiService.kt
+++ app/src/main/java/com/wjf/androidutils/net/api/ApiService.kt
@@ -2,11 +2,26 @@ package com.wjf.androidutils.net.api
 
 import com.wjf.androidutils.entity.BaseEntity
 import com.wjf.androidutils.entity.User
+import okhttp3.MultipartBody
 import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface ApiService {
 
     @GET("users")
     suspend fun getUsers(): BaseEntity<User>
+
+    @Multipart
+    @POST("check/uploadCheckPicture")
+    suspend fun uploadCheckPicture(
+        @Part imageBodyPart: MultipartBody.Part,
+        @Part bucketBodyPart: MultipartBody.Part,
+        @Part typeBodyPart: MultipartBody.Part,
+        @Part sortBodyPart: MultipartBody.Part,
+        @Part nameBodyPart: MultipartBody.Part,
+        @Part checkIdBodyPart: MultipartBody.Part
+    ): BaseEntity<String>
 
 }
